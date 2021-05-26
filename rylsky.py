a4790ef00ba322e5f325631bd1b66f2fa3ce1bf4
dae-cmyk <74119677+dae-cmyk@users.noreply.github.com>
2021-05-26 16:44:02 +0900

    Included Info and Clearer variable names

--- rylsky.py
+++ rylsky.py
@@ -1,3 +1,33 @@
+#Program informations
+
+#What this program does?
+##########################################
+#Get pictures of top rated rylsky model in page 1
+
+#Algorithm
+##########################################
+#Get Models profile link -> Get List of model's album links-> Get Album's picture links-> Download the picture's link
+
+#How to Run?
+##########################################
+#1.Initialise Constructor
+#2.Run Rylsky.ProcessLinksBeforeDownload() to get all the model's album links
+#3.After getting all the model album links, Run Rylsky.DownloadAllImages() to download all images
+
+#How it produces a folder
+##########################################
+#When it starts to download the image, 
+#At the top, it will produce a folder called "RylskyTopModels"
+#then it will produce folders according to the model's name
+#Each model folder contains their albums and have photos in it
+
+#Information about rylsky hot models page
+##########################################
+#There are 89 model profiles in rylsky top model page 1
+#One Model Profile contains approximately 10-50 albums
+#One Model Album contains approximately 10-20 pictures
+#In total, it downloads approximately 50 thousand pictures
+
 from concurrent.futures import thread
 from bs4 import BeautifulSoup
 import requests
@@ -6,72 +36,105 @@ import pdb
 import concurrent.futures as cf
 import time
 from typing import List, Union
+import sys
 
 class GetRylskyModels():
-    def __init__(self, html: str, model="models.txt"):
-        self.GetMainHtml = requests.get(html)
-        self.mainHTML = BeautifulSoup(self.GetMainHtml.content, 'lxml')
-        self.modelHTMLs: List[str] = []
-        self.redirectHTMLs: List[str] = []
-        self.modelFile = model
-    def __GetModelsHTML(self):
-        ULs = self.mainHTML.find_all('ul',class_='gallery-a a d')
-        info = []
+    def __init__(self, ModelProfileLinkDatabase="ModelAlbumLinks.txt"):
+        self.RylskyHotModelPage = "https://www.elitebabes.com/top-rated-babes/"
+        self.__ModelProfileLinks: List[str] = []
+        self.ModelNameAndAlbumLinks: List[str] = []
+        self.__ModelProfileLinkDatabase = ModelProfileLinkDatabase
+
+    #Call main html and convert to BS4 type
+    def __GetHotModelPage(self):
+        #Get the main html
+        RylskyMainHTML = requests.get(self.RylskyHotModelPage)
+
+        #When main html is received process it with bs4
+        self.__RylskyMainHTML: str = BeautifulSoup(RylskyMainHTML.content, 'lxml')
+
+    #Get model's profile links to get model's album links
+    def __GetModelProfileLinks(self):
+        #Call unordered list that stores the model's profile
+        ULs = self.__RylskyMainHTML.find_all('ul', class_='gallery-a a d')
+
+        #
+        ModelProfileLink = []
+
+        #Store model's name and profile link in list
         for ul in ULs:
+            #Find model's link by finding a href link
             As = ul.find_all('a')
+
             for a in As:
+                #Get the <span> which contains the name of the model
                 name = a.find_all('span')[-1].text
-                # print(name)
-                info = [name, a['href']]
-                # print("{}\n\n".format(a['href']))
-                self.modelHTMLs.append(info)
-    def __ReadModels(self):
-        with open(self.modelFile, 'r') as Read:
+                print("Target: Model name found! : {}".format(name))
+
+                ModelProfileLink = [name, a['href']]
+
+                self.__ModelProfileLinks.append(ModelProfileLink)
+    
+    def __ReadModelProfileLinkDB(self):
+        with open(self.__ModelProfileLinkDatabase, 'r') as Read:
             for read in Read:
                 info = Read.readline().split(',')
-                # print("{}\n".format(info))
-                self.redirectHTMLs.append(info)
-        # print(self.redirectHTMLs)
-    def __WriteModels(self):
-        with open(self.modelFile,'a') as Write:
-            for redirect in self.redirectHTMLs:
+
+                self.ModelNameAndAlbumLinks.append(info)
+
+    def __WriteModelProfileLinkDB(self):
+        with open(self.__ModelProfileLinkDatabase,'a') as Write:
+            for redirect in self.ModelNameAndAlbumLinks:
                 Write.write("{},{}\n".format(redirect[0], redirect[1]))
-    def __GetRedirectURL(self):
-        for modelHTML in self.modelHTMLs:
-            print("{}, {}\n".format(modelHTML[0], modelHTML[1]))
-            # print("Reading {}th model's website".format(i))
-            html = requests.get(modelHTML[1])
-            HTML = BeautifulSoup(html.content, 'lxml')
-            ULs = HTML.find_all('ul', class_ = 'gallery-a b')
+
+    def __GetModelAlbumLinks(self):
+        for ModelProfileLink in self.__ModelProfileLinks:
+            #Get the model's profile HTML source  
+            ModelProfile = requests.get(ModelProfileLink[1])
+            ModelProfile = BeautifulSoup(ModelProfile.content, 'lxml')
+
+            ULs = ModelProfile.find_all('ul', class_ = 'gallery-a b')
+
+            #From model's profile link, search for all the model's albums
             for ul in ULs:
+                #Find model's album links
                 As = ul.find_all('a')
-                # print(self.redirectHTMLs)
-                # sleep(10)
+
                 for a in As:
-                    info = []
-                    info.append(modelHTML[0])
-                    info.append(a['href'])
-                    # print("{}\n\n".format(info))
-                    self.redirectHTMLs.append(info)
+                    ModelNameAndAlbumLink = []
+
+                    #Name of the model
+                    ModelNameAndAlbumLink.append(ModelProfileLink[0])
+                    #Model's album link
+                    ModelNameAndAlbumLink.append(a['href'])
+
+                    print("Model Album Found!: {}".format(ModelNameAndAlbumLink[1]))
+
+                    self.ModelNameAndAlbumLinks.append(ModelNameAndAlbumLink)
+
     @staticmethod
-    def __GetImageTagsInImageHTML(redirectHTML: str) -> Union[List[str], None]:
-        imageLink = []
+    def __GetImageTagsInImageHTML(ModelAlbumLink: str) -> Union[List[str], None]:
+        AlbumImgTags: List[str] = []
         
-        #Calling Model's Collection Album html
-        html = requests.get(redirectHTML,timeout=5)
-        content = BeautifulSoup(html.content, 'lxml')
+        #Calling Model's Album Link
+        ModelAlbumLink = requests.get(ModelAlbumLink,timeout=5)
+        content = BeautifulSoup(ModelAlbumLink.content, 'lxml')
 
         ul = content.find('ul', class_='list-gallery a css')
+
+        #Find all image tags
         try:
-            #Find all image tags
-            imageTags = ul.find_all('img')
+            ModelAlbumImageTags = ul.find_all('img')
+        #If none of the img tags are found, return nothing
         except Exception:
             return None
-        #Append all the images and return
-        for tag in imageTags:
-            imageLink.append(tag)
-        return imageLink
-    def __GetImage(self, ImgLinks, dir: str) -> None:
+
+        #Append all the image tags and return the list of <img> tags
+        for ModelAlbumImageTag in ModelAlbumImageTags:
+            AlbumImgTags.append(ModelAlbumImageTag)
+        return AlbumImgTags
+
+    def __GetImage(self, ImgLinks, dir: str):
       try:
         for i , img in enumerate(ImgLinks):
               
@@ -82,12 +145,12 @@ class GetRylskyModels():
             RawImg = requests.get(img['src'], timeout=5)
 
             #Check if dir exist before copying image to the directory
-            if os.path.isdir("{0}/{1}/{2}".format('pic',dir,path)) is False:
-                os.mkdir("{0}/{1}/{2}".format('pic',dir,path))
+            if os.path.isdir("{0}/{1}/{2}".format('RylskyTopModels',dir,path)) is False:
+                os.mkdir("{0}/{1}/{2}".format('RylskyTopModels',dir,path))
 
             print("Downloading {:>90}: {:>15}th image".format(path, i))
 
-            self.__WriteImage(path="{0}/{1}/{2}/{3}.jpg".format('pic',dir, path , path+str(i)), RawImage=RawImg)
+            self.__WriteImage(path="{0}/{1}/{2}/{3}.jpg".format('RylskyTopModels',dir, path , path+str(i)), RawImage=RawImg)
       except Exception:
         pass
     
@@ -99,7 +162,7 @@ class GetRylskyModels():
           file.close()
 
     def __DownloadImages(self,start=0):
-      length = len(self.redirectHTMLs)
+      length = len(self.ModelNameAndAlbumLinks)
       i = 0
       try:
         #Call multi thread to process faster
@@ -108,21 +171,21 @@ class GetRylskyModels():
               worker.submit(self.__DownloadImage, i)
 
       except KeyboardInterrupt:
-        exit()
+        sys.exit()
 
       except Exception:
         self.__DownloadImages(i)
                       
     def __DownloadImage(self, index: int):
-          redirect = self.redirectHTMLs[index]
+          redirect = self.ModelNameAndAlbumLinks[index]
           Tags = self.__GetImageTagsInImageHTML(redirect[1])
 
           #Model's name
           name = redirect[0]
 
-          #Check if dir exist before writing getting model's pic
-          if os.path.isdir("pic/{}".format(name)) is False:
-                os.mkdir("pic/{}".format(name))
+          #Check if dir exist before writing getting model's RylskyTopModels
+          if os.path.isdir("RylskyTopModels/{}".format(name)) is False:
+                os.mkdir("RylskyTopModels/{}".format(name))
 
           #Call images from the website
           self.__GetImage(Tags, name)
@@ -150,6 +213,7 @@ class GetRylskyModels():
       except FileNotFoundError:
         return None
 
+    #Used to calculate total time taken to download all file
     @staticmethod
     def Timer(startedTime):
           time_elapsed = time.time() - startedTime
@@ -157,13 +221,13 @@ class GetRylskyModels():
           hour, min_sec = divmod(time_elapsed, 3600)
           min, sec = divmod(min_sec, 60)
           print("{:>3} hour {:>3} minute {:>3} second".format(int(hour), int(min), int(sec)))
-          
-    def GetAllImages(self):
+
+    def DownloadAllImages(self):
           start = time.time()
           try:
-            self.__ReadModels()
-            if os.path.isdir('pic') is False:
-                os.mkdir('pic')
+            self.__ReadModelProfileLinkDB()
+            if os.path.isdir('RylskyTopModels') is False:
+                os.mkdir('RylskyTopModels')
             lastRead = self.__ReadLastSection()
             if lastRead is not None:
               self.__DownloadImages(lastRead)
@@ -177,13 +241,16 @@ class GetRylskyModels():
             pass
 
     def ProcessLinksBeforeDownload(self):
-        self.__GetModelsHTML()
-        print("reading model's html done")
-        self.__GetRedirectURL()
-        print("Collecting model's picture html files done")
-        self.__WriteModels()
+        print("Running...")
+        self.__GetHotModelPage()
+        self.__GetModelProfileLinks()
+        print("\n\nFetching Model's Profile done!\n\n")
+        self.__GetModelAlbumLinks()
+        print("\n\nFetching Model's List of Albums done!\n\n")
+        self.__WriteModelProfileLinkDB()
+        print("\n\nWriting To ModelAlbumLink Done\n\n")
 
 if __name__ == '__main__':
-    html = "https://www.elitebabes.com/top-rated-babes/"
-    Rylsky = GetRylskyModels(html)
-    Rylsky.GetAllImages()
+    Rylsky = GetRylskyModels()
+    Rylsky.ProcessLinksBeforeDownload()
+    # Rylsky.DownloadAllImages()
